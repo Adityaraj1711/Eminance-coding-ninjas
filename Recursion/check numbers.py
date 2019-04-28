7583209d036f704ca8258f5b677dbda907b4400f
unknown <adityamailbox0@gmail.com>
2019-04-28 18:55:47 +0530

    change numbers

---
+++ Recursion/check numbers.py
@@ -0,0 +1,14 @@
+def checkNumber(arr, x):
+    # Please add your code here
+    pass
+
+# Main
+from sys import setrecursionlimit
+setrecursionlimit(11000)
+n=int(input())
+arr=list(int(i) for i in input().strip().split(' '))
+x=int(input())
+if checkNumber(arr, x):
+    print('true')
+else:
+    print('false')
