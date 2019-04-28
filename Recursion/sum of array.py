206155c2e12d2f41f5ce383b4dba53d48acd22e5
unknown <adityamailbox0@gmail.com>
2019-04-28 18:54:41 +0530

    array

---
+++ Recursion/sum of array.py
@@ -0,0 +1,10 @@
+def sumArray(arr):
+    # Please add your code here
+    pass
+
+# Main
+from sys import setrecursionlimit
+setrecursionlimit(11000)
+n=int(input())
+arr=list(int(i) for i in input().strip().split(' '))
+print(sumArray(arr))
