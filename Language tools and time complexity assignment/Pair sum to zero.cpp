b5c262ff541cc094088bb00dbfe6827478d4b0c9
unknown <adityamailbox0@gmail.com>
2019-05-06 17:46:33 +0530

    pair sum

---
+++ Language tools and time complexity assignment/Pair sum to zero.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void PairSum(int *input, int n){
+		/* Don't write main().
+     	* the input array is already passed as function argument.
+     	* Don't need to return anything. Print the desired pairs in the function itself.
+     	*/
+      map<int,int> um;
+      for(int i = 0;i<n;i++){
+        um[input[i]] += 1;
+      }
+  
+  
+      map<int,int> :: iterator it;
+  	for(it = um.begin();it!=um.end();it++){
+      if((*it).first>0)
+      if((*it).second>0){
+        if(um[0-(*it).first]){
+          for(int i = 0;i<((*it).second * um[0-(*it).first]);i++){
+            cout<<0-(*it).first<<" "<<(*it).first<<endl;
+          }
+        }
+      }
+    }
+      
+  
+    
+ 	}
